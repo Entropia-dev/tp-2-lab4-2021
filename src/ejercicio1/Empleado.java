66b57e19e85eae2fddb12db7d61acc2029d5b021
j Martin Povis <martin25zxc@gmail.com>
2021-04-14 18:23:59 -0300

    El equals de la clase empleado ya no compara por Id.

--- src/ejercicio1/Empleado.java
+++ src/ejercicio1/Empleado.java
@@ -87,8 +87,7 @@ public class Empleado{
 		Empleado other = (Empleado) obj;
 		if (edad != other.edad)
 			return false;
-		if (id != other.id)
-			return false;
+		//if (id != other.id) return false;
 		if (nombre == null) {
 			if (other.nombre != null)
 				return false;
